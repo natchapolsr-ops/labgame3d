762bd861a37be956be62f931588f95ea6f048344
natchapolsr-ops <natchapol.sr@kkumail.com>
2025-09-21 19:56:29 +0700

    de

---
+++ 3dmodel/Asset/demo.gd
@@ -0,0 +1,12 @@
+extends Node3D
+
+
+func _ready() -> void:
+	$Walking/AnimationPlayer.play("Locomotion-Library/run")
+	$Walking2/AnimationPlayer.play("Locomotion-Library/walk")
+	$Walking3/AnimationPlayer.play("Melee-Library--OLD/Fall")
+	$Walking4/AnimationPlayer.play("Melee-Library--OLD/StrafeRunR")
+
+
+func _process(delta: float) -> void:
+	pass
